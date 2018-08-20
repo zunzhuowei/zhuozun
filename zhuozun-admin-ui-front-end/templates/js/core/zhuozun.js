995a5b5285b7f14b6a0723a46f1b72fd49fa0f36
zunzhuowei <1808862723@qq.com>
2018-08-21 01:43:09 +0800

    add admin ui front end

---
+++ zhuozun-admin-ui-front-end/templates/js/core/zhuozun.js
@@ -0,0 +1,10 @@
+zhuozun = {
+    baseUrl: "http://192.168.1.104:7777/qs",
+    user_api: {
+        getUserById: "/user-api/user/get/",
+
+    },
+    admin_ui_api:{
+        register: "/adminUi-api/register",
+    }
+};
