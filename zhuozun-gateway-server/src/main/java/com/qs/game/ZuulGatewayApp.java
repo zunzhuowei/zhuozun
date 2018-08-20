a79bf45b1d9daec4065af1dbe6363a08ad9aab13
zunzhuowei <1808862723@qq.com>
2018-08-21 01:39:06 +0800

    add admin ui front end

--- zhuozun-gateway-server/src/main/java/com/qs/game/ZuulGatewayApp.java
+++ zhuozun-gateway-server/src/main/java/com/qs/game/ZuulGatewayApp.java
@@ -3,13 +3,9 @@ package com.qs.game;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
 /**
- 使用@EnableZuulProxy注解激活zuul。
+ * 使用@EnableZuulProxy注解激活zuul。
  * 跟进该注解可以看到该注解整合了@EnableCircuitBreaker、@EnableDiscoveryClient，是个组合注解，目的是简化配置。
  */
 @SpringBootApplication
@@ -18,18 +14,6 @@ public class ZuulGatewayApp {
 
     public static void main(String[] args) {
         SpringApplication.run(ZuulGatewayApp.class, args);
-    }
-
-    @Bean
-    public CorsFilter corsFilter() {
-        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        final CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.addAllowedOrigin("*");
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("*");
-        source.registerCorsConfiguration("/**", config);
-        return new CorsFilter(source);
     }
 
 }
