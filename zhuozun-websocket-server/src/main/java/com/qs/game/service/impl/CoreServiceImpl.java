6300b9c2e266c365b52e317415bcf0a5537c0eb4
zunzhuowei <1808862723@qq.com>
2018-09-01 23:32:33 +0800

    websocket delete wechat support

--- zhuozun-websocket-server/src/main/java/com/qs/game/service/impl/CoreServiceImpl.java
+++ zhuozun-websocket-server/src/main/java/com/qs/game/service/impl/CoreServiceImpl.java
@@ -1,12 +1,7 @@
 package com.qs.game.service.impl;
 
-import cn.binarywang.wx.miniapp.api.WxMaService;
-import cn.binarywang.wx.miniapp.bean.WxMaJscode2SessionResult;
-import cn.binarywang.wx.miniapp.bean.WxMaUserInfo;
 import com.qs.game.model.sys.Core;
 import com.qs.game.service.ICoreService;
-import me.chanjar.weixin.common.error.WxErrorException;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -15,25 +10,11 @@ import org.springframework.stereotype.Service;
 @Service
 public class CoreServiceImpl implements ICoreService {
 
-    @Autowired
-    private WxMaService wxService;
 
     @Override
     public boolean checkParams(Core core) {
         return false;
     }
 
-    public void test() throws WxErrorException {
-        WxMaJscode2SessionResult result = wxService.getUserService().getSessionInfo("");
-        result.getOpenid();
-        result.getSessionKey();
-        result.getUnionid();
-
-        WxMaUserInfo userInfo = wxService.getUserService().getUserInfo("", "", "");
-        userInfo.getCity();
-        userInfo.getOpenId();
-        userInfo.getUnionId();
-
-    }
 
 }
