c319df7af5792bf5c1be715aeebee91a2b8e7a19
zunzhuowei <1808862723@qq.com>
2018-09-04 18:58:33 +0800

    心跳检测重构

--- zhuozun-websocket-server/src/main/java/com/qs/game/handler/HeartbeatHandler.java
+++ zhuozun-websocket-server/src/main/java/com/qs/game/handler/HeartbeatHandler.java
@@ -10,6 +10,7 @@ import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.channel.socket.ChannelInputShutdownReadComplete;
 import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
 import io.netty.handler.ssl.SslHandshakeCompletionEvent;
+import io.netty.handler.timeout.IdleState;
 import io.netty.handler.timeout.IdleStateEvent;
 import io.netty.util.ReferenceCountUtil;
 import lombok.extern.slf4j.Slf4j;
@@ -37,6 +38,7 @@ public class HeartbeatHandler extends ChannelInboundHandlerAdapter {
     public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
         log.debug("HeartbeatHandler userEventTriggered --::{},{}", ctx, evt);
         if (evt instanceof IdleStateEvent) { //心跳事件
+            /*
             String key = CacheKey.RedisPrefix.USER_HEART_BEAT.KEY;
             String longId = ctx.channel().id().asLongText();
             Long incr = redisService.incrOne(key + longId);
@@ -49,6 +51,17 @@ public class HeartbeatHandler extends ChannelInboundHandlerAdapter {
             }
             ctx.writeAndFlush(new TextWebSocketFrame("HB"))
                     .addListener(ChannelFutureListener.CLOSE_ON_FAILURE);
+            */
+
+            IdleState state = ((IdleStateEvent) evt).state();
+            if (state == IdleState.READER_IDLE) {
+                String longId = ctx.channel().id().asLongText();
+                String uid = ctx.channel().attr(Global.attrUid).get();
+                //oolean b = global.delChannelFromGroup(uid, ctx.channel());
+                //ctx.channel().close();
+                //log.info("channel id {} is death ! uid = {} , result = {} ", longId, uid, b);
+            }
+
         } else if (evt instanceof SslHandshakeCompletionEvent) { //ssl 事件
             log.info("HeartbeatHandler userEventTriggered SslHandshakeCompletionEvent --::{}", evt);
         } else if (evt instanceof ChannelInputShutdownReadComplete) {//频道已关闭
