e35ada8b7da1e516344b7f746c50f1befd53f62b
zunzhuowei <1808862723@qq.com>
2018-11-17 18:00:22 +0800

    spring web socket init commit

---
+++ zhuozun-springwebsocket/src/main/java/com/qs/game/SpringWebSocketApp.java
@@ -0,0 +1,18 @@
+package com.qs.game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by zun.wei on 2018/11/17 17:19.
+ * Description:
+ */
+@SpringBootApplication
+public class SpringWebSocketApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebSocketApp.class, args);
+    }
+
+}
+
