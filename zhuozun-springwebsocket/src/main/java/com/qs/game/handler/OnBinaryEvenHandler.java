52a3d23423247f17b9d66c09417486ffcf5d0ffa
zunzhuowei <1808862723@qq.com>
2018-12-13 11:27:39 +0800

    update binary handler

--- zhuozun-springwebsocket/src/main/java/com/qs/game/handler/OnBinaryEvenHandler.java
+++ zhuozun-springwebsocket/src/main/java/com/qs/game/handler/OnBinaryEvenHandler.java
@@ -52,7 +52,8 @@ public class OnBinaryEvenHandler implements EvenHandler {
 //        System.out.println("str = " + str);
 //        System.out.println("tel = " + tel);
 
-        sysWebSocket.sendMessage(ByteUtils.beginBuild().append(q).append(s).append(WEB_SOCKET_MAP.size()).buildByteArr());
+        sysWebSocket.sendMessage(ByteUtils.beginBuild().append(q).append(s)
+                .append(Integer.parseInt(sid)).append(WEB_SOCKET_MAP.size()).buildByteArr());
 
         message.clear();
     }
