2c77fe083f3a21e02cc062ad48f7648c658a4a03
zunzhuowei <1808862723@qq.com>
2018-12-28 16:44:43 +0800

    update

--- zhuozun-springwebsocket/src/test/java/com/qs/game/sh/ShellTest4Git.java
+++ zhuozun-springwebsocket/src/test/java/com/qs/game/sh/ShellTest4Git.java
@@ -25,7 +25,7 @@ public class ShellTest4Git {
         //ServerEnumGit serverEnumGit = ServerEnumGit.getServerEnumGit(ServerEnumGit.TEST_SERVER_ACTI_HAPPY_BEARD);
         //ServerEnumGit serverEnumGit = ServerEnumGit.getServerEnumGit(ServerEnumGit.TEST_SERVER_APP_HAPPY_BEARD);
 
-        ServerEnumGit serverEnumGit = ServerEnumGit.getServerEnumGit(ServerEnumGit.TEST_SERVER_WEB_MINI_DDZ);
+        //ServerEnumGit serverEnumGit = ServerEnumGit.getServerEnumGit(ServerEnumGit.TEST_SERVER_WEB_MINI_DDZ);
         //ServerEnumGit serverEnumGit = ServerEnumGit.getServerEnumGit(ServerEnumGit.TEST_SERVER_ACTI_MINI_DDZ);
         //ServerEnumGit serverEnumGit = ServerEnumGit.getServerEnumGit(ServerEnumGit.TEST_SERVER_APP_MINI_DDZ);
 
@@ -37,6 +37,10 @@ public class ShellTest4Git {
         //ServerEnumGit serverEnumGit = ServerEnumGit.getServerEnumGit(ServerEnumGit.ONLINE_SERVER_WEB_HAPPY_BEARD);
         //ServerEnumGit serverEnumGit = ServerEnumGit.getServerEnumGit(ServerEnumGit.ONLINE_SERVER_ACTI_HAPPY_BEARD);
         //ServerEnumGit serverEnumGit = ServerEnumGit.getServerEnumGit(ServerEnumGit.ONLINE_SERVER_APP_HAPPY_BEARD);
+
+        //ServerEnumGit serverEnumGit = ServerEnumGit.getServerEnumGit(ServerEnumGit.ONLINE_SERVER_WEB_MINI_DDZ);
+        ServerEnumGit serverEnumGit = ServerEnumGit.getServerEnumGit(ServerEnumGit.ONLINE_SERVER_ACTI_MINI_DDZ);
+        //ServerEnumGit serverEnumGit = ServerEnumGit.getServerEnumGit(ServerEnumGit.ONLINE_SERVER_APP_MINI_DDZ);
 
         File file = new File("temp.txt");
         PrintStream ps = new PrintStream(new FileOutputStream(file));
