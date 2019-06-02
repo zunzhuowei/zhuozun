b036fe7829d32262ab0383a35f4f0f68daf20450
zunzhuowei <1808862723@qq.com>
2019-06-02 23:55:00 +0800

    spring shell 初体验 init commit

---
+++ zhuozun-shell/src/main/java/com/qs/game/SpringShellApp.java
@@ -0,0 +1,18 @@
+package com.qs.game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by zun.wei on 2019/6/2.
+ */
+@SpringBootApplication
+public class SpringShellApp {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringShellApp.class, args);
+    }
+
+
+}
---
+++ zhuozun-shell/src/main/java/com/qs/game/translation/TranslationCommands.java
@@ -0,0 +1,35 @@
+package com.qs.game.translation;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
+import org.springframework.shell.standard.ShellOption;
+
+import java.util.Locale;
+
+/**
+ * Created by zun.wei on 2019/6/2.
+ */
+@ShellComponent
+public class TranslationCommands {
+
+
+    /*
+    shell:>translate "hello world!" --from en_US --to fr_FR
+    hello world!:en_US:fr_FR
+    shell:>
+     */
+
+    @ShellMethod("Translate text from one language to another.")
+    public String translate(
+            @ShellOption String text,
+            @ShellOption( defaultValue = "en_US") Locale from,
+            @ShellOption() String to) {
+
+        // invoke service
+        //return service.translate(text, from, to);
+
+        return text + ":" + from + ":" + to;
+    }
+
+}
